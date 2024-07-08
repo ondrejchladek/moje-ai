211d1b1d920ec7f2d26c4adcdc105a50749676f4
ondrejchladek <ondrejchladek@gmail.com>
2024-07-09 01:50:48 +0200

    modified:   lib/database/mongoose.ts 	modified:   middleware.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'moje_ai', bufferCommands: false 
+      dbName: 'imaginify', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,19 @@
-import { authMiddleware } from "@clerk/nextjs/server";
- 
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+// middleware.ts
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// Define public routes
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/api/webhooks/clerk',
+  '/api/webhooks/stripe',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
